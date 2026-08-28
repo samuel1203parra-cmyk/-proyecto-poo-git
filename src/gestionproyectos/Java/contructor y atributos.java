d64cf4f0b738e3900ac4b9f327fe73e5db8c85e7
Samuel Parra Trujillo <separrat@udistrital.edu.co>
2026-08-27 20:26:41 -0500

    feat: agrega carpeta java con clase y constructor

---
+++ src/gestionproyectos/Java/contructor y atributos.java
@@ -0,0 +1,21 @@
+public class Tarea  {
+ private int peso;
+ private String marca;
+ private int Nbolsillos;
+ private String color;
+ private int tamano; 
+ private int Maxpeso; 
+
+  public Tarea(int peso, String marca, int Nbolsillos, String color, int tamano, int Maxpeso){
+
+  this.peso=peso;
+  this.marca=marca;
+  this.Nbolsillos=Nbolsillos; 
+  this.color=color;
+  this.tamano=tamano;
+  this.Maxpeso=Maxpeso;
+
+ }
+
+
+}
